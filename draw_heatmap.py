57a020327ae3d6cc42ed689eeddacf53e4b8124d
RokurouIchihara <s224416t@st.go.tuat.ac.jp>
2022-10-31 08:00:51 +0900

    [add] make heat map func

--- draw_heatmap.py
+++ draw_heatmap.py
@@ -3,6 +3,7 @@ import sys
 import os
 import csv
 import cv2
+import numpy as np
 from PIL import Image
 from pprint import pprint
 
@@ -28,11 +29,14 @@ def main():
     # 画像毎にヒートマップ作成
     for image in images:
         with open(img_to_csv[image], 'r') as f:
-            gazedatas = get_fixed_gaze_data(width, image, list(csv.reader((f)))[1:])
+            gazedatas = get_fixed_gaze_data(
+                width, image, list(csv.reader((f)))[1:])
             draw_heatmap(image, gazedatas)
-            
+            return
 
 # 横のずれ直し
+
+
 def get_fixed_gaze_data(width, image, gazedatas):
     datas = []
     img = Image.open(image)
@@ -69,17 +73,40 @@ def rescale_images(images, width, height):
         img_resize.save(image, quality=100)
 
 # ヒートマップ作成
+
+
 def draw_heatmap(filename, gazedatas):
     img = cv2.imread(filename)
-    annotated = img.copy()
+    print(filename)
+    annotated = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
+
+    # 真っ黒な画像を生成
+    black_img = np.zeros(annotated.shape, np.uint8)
+    for j, row in enumerate(black_img):
+        for i, column in enumerate(row):
+            black_img[j][i][3] = 255
+    # 計算用
+    cal_img = black_img.copy()
+    cal_img = cal_img.astype(np.int64)
+
+    # 円でくり抜くところをカウント
     for gazedata in gazedatas:
-        cv2.circle(annotated, tuple(gazedata), 40, color=(
-            100, 100, 100), thickness=-1)
-    mat_img = cv2.addWeighted(annotated, 0.4, img, 0.6, 0)
-    #cv2.imshow('', img)
-    #cv2.waitKey(0)
-    #cv2.imwrite(filename.replace('fig/', 'heatmap/').replace('.png', '_heatmap.png'), mat_img)
-    cv2.imwrite(filename.replace('fig/', 'heatmap/'), mat_img)
+        for i in range(1, 16):
+            for_add = black_img.copy()
+            cv2.circle(for_add, tuple(gazedata), 5*i, color=(
+                10, 10, 10), thickness=-1)
+            cal_img += for_add
+    alphas = np.full(img.shape, 0.0, dtype=float)
+    for j in range(len(cal_img)):
+        for i in range(len(cal_img[j])):
+            alphas[j][i] = np.array(
+                [val / 255 if val < 255 else 1.0 for val in cal_img[j][i][:3]])
+    for j in range(len(annotated)):
+        for i in range(len(annotated[j])):
+            annotated[j][i] = annotated[j][i] * alphas[j][i][0] + \
+                black_img[j][i]*(1-alphas[j][i][0])
+    cv2.imshow('', annotated)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
