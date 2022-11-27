75531552ee1bcd0015014d1e4b417ce5a93cf9db
Rokichi <s224416t@st.go.tuat.ac.jp>
2022-11-28 04:53:04 +0900

    [add] convert_dcm_2_png

--- get_dicom_file.py
+++ get_dicom_file.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import cv2
+import os
 import pydicom
+from matplotlib import pyplot as plt
+from PIL import Image
 
-filename = "C:\\Users\\byrk1\AppData\\Local\\Google\\Cloud SDK\\p10\\p10000032\\s50414267\\02aa804e-bde0afdd-112c0b34-7bc16630-4e384014.dcm"
 
 def main():
-    path = pydicom.data.get_testdata_file(filename)
-    dataset = pydicom.filereader.dcmread(path)
-    img = dataset.pixel_array
-    cv2.imwrite(img, filename.replace('.dcm', '.png'))
+    files = os.listdir('dicom')
+    os.chdir('dicom')
+    for filename in files:
+        dataset = pydicom.read_file(filename)
+        img = dataset.pixel_array
+        gray_img = Image.fromarray((img/16).astype('uint8'), mode = 'L')
+        gray_img.save(filename.replace('.dcm', '.png'))
+
 
 if __name__ == '__main__':
     main()
